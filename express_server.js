4b2db8ea99d743604d9ef2e45328677c69bd0cd5
Marcus Dias <mvrdias@uol.com.br>
2017-10-10 21:38:52 +0000

    Initial commit with simple server
    This is a long message to demonstrate the use of longer commit messages.  Ignore this, I'm just playin' around.

---
+++ express_server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+//new route handler for "/urls" and use res.render()
+//to pass the URL data to your template.
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
