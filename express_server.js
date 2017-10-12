13f64f6fd085f175646c5ba12b7d935f4a71ed8a
Marcus Dias <mvrdias@uol.com.br>
2017-10-12 02:13:35 +0000

    Delete and Update functions

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Step by Step - Duration
+// app.use((req, res, next) => {
+//   const ts = new Date().getTime();
+//   next();
+//   const after = ts - new Date().getTime();
+//   console.log('Request took %s ms', after)
+// })
 
 app.set("view engine", "ejs");
 
@@ -10,24 +22,77 @@ var urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.redirect("/urls");
 });
 
 //new route handler for "/urls" and use res.render()
 //to pass the URL data to your template.
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
+});
+
+// urls_show route
+app.get("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let longURL  = urlDatabase[shortURL];
+  let templateVars = { shortURL, longURL };
+  res.render("urls_show", templateVars);
+});
+
+
+// This code should output any request parameters to your terminal.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+// to handle shortURL requests
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  // let longURL = ... "/u/:shortURL" that redirects to its longURL ???
+  res.redirect(longURL);
+});
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+//Delete Informations
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+
+//Update Informations
+app.post("/urls/:shortURL/update", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const newURL  = req.body.longURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
+function generateRandomString() {
+  var letras = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+  var aleatorio = '';
+  for (var i = 0; i < tamanho; i++) {
+      var rnum = Math.floor(Math.random() * letras.length);
+      aleatorio += letras.substring(rnum, rnum + 1);
+  }
+  return aleatorio;
+}
---
+++ teste.js
@@ -0,0 +1,11 @@
+function textoAleatorio(tamanho)
+{
+    var letras = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+    var aleatorio = '';
+    for (var i = 0; i < tamanho; i++) {
+        var rnum = Math.floor(Math.random() * letras.length);
+        aleatorio += letras.substring(rnum, rnum + 1);
+    }
+    return aleatorio;
+}
+console.log(textoAleatorio (6));
